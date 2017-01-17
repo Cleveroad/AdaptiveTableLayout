7b176fcdec00871de92492868bef84f7aadcbe8c
ilchenko_cr <illechenko.cr@gmail.com>
2017-01-17 15:50:54 +0200

    fix: cell width and height in the low-resolution devices fix: NullPointer in the CsvUtils project refactoring

--- sample/src/main/java/com/cleveroad/tablelayout/datasource/UpdateFilecallback.java
+++
@@ -1,7 +0,0 @@
-package com.cleveroad.tablelayout.datasource;
-
-import android.support.annotation.Nullable;
-
-public interface UpdateFilecallback {
-    void onFileUpdated(String fileName, boolean isSuccess, @Nullable Throwable throwable);
-}
