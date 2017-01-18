c6cbf4bb1c3b6e08121e1309d003d45d6adbe962
ilchenko_cr <illechenko.cr@gmail.com>
2017-01-18 19:20:00 +0200

    hotfix: string utils

--- sample/src/main/java/com/cleveroad/tablelayout/utils/StringUtils.java
+++ sample/src/main/java/com/cleveroad/tablelayout/utils/StringUtils.java
@@ -1,18 +1,10 @@
 package com.cleveroad.tablelayout.utils;
 
-import android.support.annotation.NonNull;
-import android.text.TextUtils;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 public class StringUtils {
     public static String toString(Iterable iterable, String separator) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        Collection<CharSequence> collection = filterNulls(iterable);
-        for (CharSequence item : collection) {
+        for (Object item : iterable) {
             stringBuilder.append(String.valueOf(item));
             stringBuilder.append(separator);
         }
@@ -20,19 +12,5 @@ public class StringUtils {
         if (stringBuilder.length() > 2) {
             return stringBuilder.substring(0, stringBuilder.length() - separator.length());
         } else return stringBuilder.toString();
-    }
-
-    @NonNull
-    public static List<CharSequence> filterNulls(Iterable iterable) {
-        List<CharSequence> result = new ArrayList<>();
-        for (Object o : iterable) {
-            if (o != null) {
-                String string = String.valueOf(o);
-                if (!TextUtils.isEmpty(string)) {
-                    result.add(string);
-                }
-            }
-        }
-        return result;
     }
 }
